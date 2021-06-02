b63a3f590ae3ba11da6d5c1a718204d1d1a7b0bf
lelong.ll32@gmail.com <lelong.ll32@gmail.com>
2021-06-03 02:05:12 +0700

    Thuc hanh ArrayList, LinkedList

--- A0121I1-Module2/10 DSA Danh sach/Thuc hanh/src/demo_my_list/MyListTest.java
+++ A0121I1-Module2/10 DSA Danh sach/Thuc hanh/src/demo_my_list/MyListTest.java
@@ -2,7 +2,7 @@ package demo_my_list;
 
 public class MyListTest {
     public static void main(String[] args) {
-        MyList<Integer> listInteger = new MyList<Integer>();
+        MyList<Integer> listInteger = new MyList<>();
         listInteger.add(1);
         listInteger.add(2);
         listInteger.add(3);
@@ -12,10 +12,6 @@ public class MyListTest {
         System.out.println("element 5: " + listInteger.get(4));
         System.out.println("element 2: " + listInteger.get(1));
         System.out.println("element 3: " + listInteger.get(2));
-
-//        listInteger.get(6);
-//        System.out.println("element 6: " + listInteger.get(6));
-
         listInteger.get(-1);
         System.out.println("element -1: " + listInteger.get(-1));
     }
